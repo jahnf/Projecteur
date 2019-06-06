9e29de8b029bd1f69be3ed575280d11e99b6867f
Jahn <jahnf@users.noreply.github.com>
2019-06-06 16:22:22 +0200

    Reload udev rules via udevadm as post installation step. #17

--- cmake/modules/PkgDependenciesProjecteur.cmake
+++ cmake/modules/PkgDependenciesProjecteur.cmake
@@ -2,18 +2,21 @@ list(APPEND _PkgDeps_Projecteur_opensuse
   "libqt5-qtgraphicaleffects >= 5.6"
   "libQt5Widgets5 >= 5.6"
   "shadow"
+  "udev"
 )
 
 list(APPEND _PkgDeps_Projecteur_debian
   "qml-module-qtgraphicaleffects (>= 5.6)"
   "libqt5widgets5 (>= 5.6)"
   "passwd"
+  "udev"
 )
 
 list(APPEND _PkgDeps_Projecteur_ubuntu_1604
   "qml-module-qtgraphicaleffects (>= 5.5)"
   "libqt5widgets5 (>= 5.5)"
   "passwd"
+  "udev"
 )
 
 list(APPEND PkgDependencies_MAP_Projecteur
