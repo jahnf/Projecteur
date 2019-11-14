40fa407c3b8d9cf6856f7979b5cf616407a55b05
Jahn <jahnf@users.noreply.github.com>
2019-11-14 17:21:40 +0100

    Show warning in preferences dialog if no compositing manager is running #24 #10 #50 #37

--- cmake/modules/PkgDependenciesProjecteur.cmake
+++ cmake/modules/PkgDependenciesProjecteur.cmake
@@ -1,12 +1,14 @@
 list(APPEND _PkgDeps_Projecteur_opensuse
   "libqt5-qtgraphicaleffects >= 5.7"
   "libQt5Widgets5 >= 5.7"
+  "libQt5X11Extras5 >= 5.7"
   "shadow"
   "udev"
 )
 
 list(APPEND _PkgDeps_Projecteur_fedora
   "qt5 >= 5.7"
+  "qt5-qtx11extras >= 5.7"
   "passwd"
   "udev"
 )
@@ -14,6 +16,7 @@ list(APPEND _PkgDeps_Projecteur_fedora
 list(APPEND _PkgDeps_Projecteur_debian
   "qml-module-qtgraphicaleffects (>= 5.7)"
   "libqt5widgets5 (>= 5.7)"
+  "libqt5x11extras5 (>= 5.7)"
   "passwd"
   "udev"
 )
@@ -22,6 +25,7 @@ list(APPEND _PkgDeps_Projecteur_archlinux
   "qt5-base>=5.7"
   "qt5-declarative>=5.7"
   "qt5-graphicaleffects>=5.7"
+  "qt5-x11extras>=5.7"
   "udev"
 )
 
